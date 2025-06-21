ac18f25a7229794be0b7524ee5b2b68ba63d180a
AdamT <a.tupilagov@gmail.com>
2025-06-21 15:32:21 +0300

    TestNG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,37 @@
 package ru.netology.service;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldReturn100IfAmountIs900() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(900);
+        int expected = 100;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn0IfAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn1IfAmountIs999() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(999);
+        int expected = 1;
+
+        Assert.assertEquals(actual, expected);
+    }
 }
