8f34722b97d86efefa86103a204c193c34647fb8
AdamT <a.tupilagov@gmail.com>
2025-06-22 13:58:27 +0300

    testtt

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -34,4 +34,14 @@ public class CashbackHackServiceTest {
 
         Assert.assertEquals(actual, expected);
     }
+
+    @Test
+    public void shouldReturn0IfAmountIsMultipleOf1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        Assert.assertEquals(expected, actual);
+    }
 }
